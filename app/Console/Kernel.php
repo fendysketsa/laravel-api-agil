6bf83ad704e31372c4996439031aa6b7ca3d75e3
fendysketsa <fendysketsa@gmail.com>
2021-08-13 23:58:58 +0700

    Done ...

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,8 +27,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
 
-        $schedule->command('schedule:time')
-            ->daily();
+        $schedule->command('schedule:time')->everyMinute();
     }
 
     /**
