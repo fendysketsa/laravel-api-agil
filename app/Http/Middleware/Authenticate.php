941c60fd505204effb6e8ea133f6627634ef34d3
fendysketsa <fendysketsa@gmail.com>
2021-08-15 01:10:11 +0700

    Update maning

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,9 +14,8 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        if (!$request->expectsJson()) {
-            return response()->json(['message' => 'Your Header False']);
-            // return route('login');
+        if (! $request->expectsJson()) {
+            return route('login');
         }
     }
 }
